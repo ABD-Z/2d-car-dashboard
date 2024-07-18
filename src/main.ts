3bf97e0b4e6fe7182fcfd1b1466d4f957af865fd
Abdulmajid, Olivier NASSER <aboud2008@gmail.com>
2024-07-18 14:50:48 +0200

    Import CSS and export all types

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
 import "leaflet/dist/leaflet.css";
+import "./vehicleDashboard.css"
 export { VehicleDashboard } from './vehicleDashboard'
+export type  {
+    UINStatus,
+    ValueUnit,
+    ValueUnitsDic,
+    VehicleDatas,
+    VehicleDatasInterface,
+    DataTypes,
+} from './vehicleDashboard.types';
